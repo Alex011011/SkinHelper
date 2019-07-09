423e940258e33f8ff8163627e5965076d89b9086
Atrius <atriusedm@gmail.com>
2019-07-09 00:48:05 -0400

    Style upgrade, convert images into canvas array

--- script.js
+++ script.js
@@ -1,64 +1,86 @@
+// Page setup
 var canvas, ctx;
-var canv = document.createElement('canvas');
-
 function load() {
-    var imageLoader = document.getElementById('imageLoader');
-    imageLoader.addEventListener('change', handleImage, false);
     canvas = document.getElementById('imageCanvas');
-    ctx = canvas.getContext('2d');
+    ctx    = canvas.getContext('2d');
+    document.getElementById('imageLoader')
+            .addEventListener('change', handleImage, false);
 }
 
+// Read and convert skins
+var skins = [];
 function handleImage(e) {
     const files = e.currentTarget.files;
-    var x = 0, y = 0;
+    var x = 0,  y = 0;
     canvas.height = 64;
+    // Loop over all selected files
     Object.keys(files).forEach(i => {
-        const file = files[i];
+        const file   = files[i];
         const reader = new FileReader();
+        // Process files once the reader is loaded
         reader.onload = (event) => {
-            // TODO canvas size management and image processing
             var img = new Image()
             img.src = event.target.result;
             img.onload = () => {
-                ctx.drawImage(img, 
+                // Compartmentalize each skin into it's own canvas
+                var skin    = document.createElement('canvas');
+                var skinCtx = skin.getContext('2d');
+                // Draw the skin into the canvas, then convert and store it
+                skinCtx.drawImage(img, 0, 0);
+                convertImage(skinCtx);
+                skins.push(skin);
+                // Draw the converted image into the webpage in a grid-like fashion
+                ctx.drawImage(skin,
                     (x == 0 ? x++ : x++ * 64) + 10, 
                     (y == 0 ? 0   : y   * 64) + 10
                 );
-
+                // Build the main canvas in rows of five
                 if (x % 5 == 0) {
-                    x = 0;
-                    y++;
+                    x = 0, y++;
                     redraw();
                 }
-                convertImage(x, y);
             }
         }
+        // Start reading the input data
         reader.readAsDataURL(file);
     });
 }
 
-function convertImage(offsetX, offsetY) {
-    //blank data
-    var temp = ctx.getImageData(46, 52, 2, 12);
+function convertImage(context) {
+    // Blank data
+    var temp = context.getImageData(46, 52, 2, 12);
     
     for (i = 0; i < temp.data.length; i++) {
         temp.data[i] = 0;
     }
-    //right arm, 1st layer
-    ctx.putImageData(ctx.getImageData(37, 48, 11, 16), 36, 48);
-    ctx.putImageData(ctx.getImageData(41, 48, 3, 4), 40, 48);
-    ctx.putImageData(temp, 46, 52);
-    //right arm, 2nd layer
-    ctx.putImageData(ctx.getImageData(53, 48, 11, 16), 52, 48);
-    ctx.putImageData(ctx.getImageData(57, 48, 3, 4), 56, 48);
-    ctx.putImageData(temp, 62, 52);
-    //left arm, 1st layer
-    ctx.putImageData(ctx.getImageData(48, 16, 9, 16), 47, 16);    
-    ctx.putImageData(ctx.getImageData(50, 16, 2, 4), 49, 16);
-    ctx.putImageData(ctx.getImageData(52, 20, 4, 12), 51, 20);
-    //left arm, 2nd layer
-    ctx.putImageData(ctx.getImageData(48, 36, 9, 16), 47, 36);    
-    ctx.putImageData(ctx.getImageData(52, 36, 4, 12), 51, 36);
+    // Right arm, 1st layer
+    context.putImageData(context.getImageData(41, 48, 3,  4),  40, 48);
+    context.putImageData(context.getImageData(37, 48, 11, 16), 36, 48);
+    context.putImageData(temp, 46, 52);
+    // Right arm, 2nd layer
+    context.putImageData(context.getImageData(53, 48, 11, 16), 52, 48);
+    context.putImageData(context.getImageData(57, 48, 3,  4),  56, 48);
+    context.putImageData(temp, 62, 52);
+    // Left arm, 1st layer
+    context.putImageData(context.getImageData(48, 16, 9,  16), 47, 16);    
+    context.putImageData(context.getImageData(50, 16, 2,  4),  49, 16);
+    context.putImageData(context.getImageData(52, 20, 4,  12), 51, 20);
+    // Left arm, 2nd layer
+    context.putImageData(context.getImageData(48, 36, 9,  16), 47, 36);    
+    context.putImageData(context.getImageData(52, 36, 4,  12), 51, 36);
+}
+
+// Redraw the canvas as needed when the canvas is resized.
+var canv = document.createElement('canvas'), tempCtx = canv.getContext('2d');
+function redraw() {
+    // Set the temp canvas size
+    canv.width  = canvas.width;
+    canv.height = canvas.height;
+    // Copy the canvas into the temp storage variable
+    tempCtx.drawImage(canvas, 0, 0);
+    canvas.height += 74;
+    // Redraw canvas from storage after resizing
+    ctx.drawImage(canv, 0, 0);
 }
 
 function downloadImg() {
@@ -68,12 +90,4 @@ function downloadImg() {
     
     download.setAttribute("href", image);
     //download.setAttribute("download","archive.png");
-}
-
-function redraw() {
-    canv.width = canvas.width;
-    canv.height = canvas.height;
-    canv.getContext('2d').drawImage(canvas, 0, 0);
-    canvas.height += 74;
-    ctx.drawImage(canv, 0, 0);
 }
